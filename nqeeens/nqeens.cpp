a9ad6c55f4f5e2ec8ba2645b62be3cb2faf4c9ad
Jung Hoon Sohn <junghoon.sohn@samsung.com>
2015-03-26 19:13:30 +0900

    Changes to be committed: 	modified:   nqeens.cpp

--- nqeeens/nqeens.cpp
+++ nqeeens/nqeens.cpp
@@ -3,10 +3,15 @@
 #include <utility>
 #include <cmath>
 
+//#include <chrono>
+
 #define PLIST vector< pair<int, int> > 
 #define PAIR	pair<int, int> 
 
 using namespace std;
+
+using namespace std::chrono;
+
 int N = 1;
 
 bool is_qpos(PLIST queens, PAIR qpos)
@@ -28,10 +33,18 @@ int get_possible_qpos(PLIST queens, int depth)
   else if((depth == N) && (queens.size() == N))
     return 1;
   else{
+    char possible_qpos[12] = {0};
+    for(PLIST::iterator it = queens.begin(); it != queens.end(); it++){
+      possible_qpos[it->first] = 1; 
+      int diff = abs(depth-it->second);
+      if((it->first - diff) > -1) possible_qpos[it->first-diff] = 1;
+      if((it->first + diff) < N) possible_qpos[it->first+diff] = 1;
+    }
+
     int sum = 0;
     for(int i = 0; i < N; i++){
       PAIR tmp(i, depth);
-      if(is_qpos(queens, tmp)){
+      if(possible_qpos[i] != 1){
         queens.push_back(tmp);
         sum += get_possible_qpos(queens, depth+1);
         queens.pop_back();
@@ -48,6 +61,9 @@ int main()
 
 	for(int i = 0; i < testcase; i++){
     cin >> N;
+
+    //high_resolution_clock::time_point t1 = high_resolution_clock::now();
+
     PLIST queens;
     int sum = 0;
     for(int j = 0; j < N; j++){
@@ -56,6 +72,10 @@ int main()
       sum += partial_sum;
       queens.pop_back();
     }
+
+    //high_resolution_clock::time_point t2 = high_resolution_clock::now();
+    //cout << "execution time = " << duration_cast<milliseconds>(t2-t1).count() << endl;
+
     printf("%d\n", sum);
   }
 }

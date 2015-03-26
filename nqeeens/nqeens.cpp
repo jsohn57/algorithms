4b3a1952a2b7e8b6b52cb3de2fa79128149f3c76
Jung Hoon Sohn <junghoon.sohn@samsung.com>
2015-03-26 15:31:32 +0900

    nqeens problem ver.1.0

--- nqeeens/nqeens.cpp
+++ nqeeens/nqeens.cpp
@@ -1,20 +1,44 @@
 #include <iostream>
 #include <vector>
 #include <utility>
-#include <math>
+#include <cmath>
 
-#define PLIST vector<pair<int, int>> 
-#define PAIR	pair<int int> 
+#define PLIST vector< pair<int, int> > 
+#define PAIR	pair<int, int> 
+
 using namespace std;
-int N;
-PLIST check_nth_qpos(PAIR qpos, int depth)
+int N = 1;
+
+bool is_qpos(PLIST queens, PAIR qpos)
 {
-	PLIST qlist; 
 	// depth = i in (i,j)
-	for(int j = 0; j < len; j++){
-		if((qpos.second == j) || abs((qpos.first-depth) != abs(qpos.second-j)))
+  for(PLIST::iterator it = queens.begin(); it != queens.end(); it++){
+    //printf("inspecting : (%d, %d) vs (%d, %d)\n", qpos.first, qpos.second, it->first, it->second);
+		if((qpos.first == it->first) || (abs(qpos.first-it->first) == abs(qpos.second-it->second)))
 			return false;
-	}
+  }
+  return true;
+}
+
+int get_possible_qpos(PLIST queens, int depth)
+{
+  // basecase
+  if(depth > N)
+    return 0;
+  else if((depth == N) && (queens.size() == N))
+    return 1;
+  else{
+    int sum = 0;
+    for(int i = 0; i < N; i++){
+      PAIR tmp(i, depth);
+      if(is_qpos(queens, tmp)){
+        queens.push_back(tmp);
+        sum += get_possible_qpos(queens, depth+1);
+        queens.pop_back();
+      }
+    }
+    return sum;
+  }
 }
 
 int main()
@@ -22,7 +46,16 @@ int main()
 	int testcase;
 	cin >> testcase;
 
-	for(int i = 0; i < testcase){
-
-	}
+	for(int i = 0; i < testcase; i++){
+    cin >> N;
+    PLIST queens;
+    int sum = 0;
+    for(int j = 0; j < N; j++){
+      queens.push_back(PAIR(j,0));
+      int partial_sum = get_possible_qpos(queens, 1);
+      sum += partial_sum;
+      queens.pop_back();
+    }
+    printf("%d\n", sum);
+  }
 }

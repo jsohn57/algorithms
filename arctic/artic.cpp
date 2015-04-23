46757793d89f1cf53eb20a96dd9e3288542bdd0e
Jung Hoon Sohn <jsohn57@gmail.com>
2015-04-23 10:57:08 +0900

    artic initial version  On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	modified:   artic.cpp

--- arctic/artic.cpp
+++ arctic/artic.cpp
@@ -3,41 +3,49 @@
 #include <utility>
 #include <algorithm>
 #include <cmath>
+#include <cstring>
 
 using namespace std;
 
 #define PAIR pair<double, double>
 #define MP(x,y) make_pair(x,y)
 
-PAIR NL[100];
-double DP[100][100];
+int N = 0;
+PAIR NL[101];
+int DP[101][101];
 
+void print(vector<int> VL)
+{
+	printf("VL = ");
+	for(vector<int>::iterator itr = VL.begin(); itr != VL.end(); itr++) printf("%d ", *itr);
+	printf("\n");
+}
 double path(int src, int dst, vector<int> VL)
 {
   if(dst == 0){
-    DP[src][dst] = 0;
-    return DP[src][dst];
+		DP[src][dst] = 0;
+		return 0;
   }
   // i = next src indicies
-  int min = 1500;
-  for(int i = 0; i < 100; i++){
-    if(i != dst){
-      bool contains = false;
-      for(vector<int>::iterator itr = VL.begin(); itr != VL.end(); itr++){
-        if(*itr == i){contains = true; break;}
-      }
-      if(!contains){
-        VL.push_back(i); 
-        double tmp = 0;
-        if(DP[i][dst] == -1.0)
-          tmp = max(sqrt(pow(NL[i].first-NL[dst].first, 2.0) + pow(NL[i].second-NL[dst].second, 2.0)), path(0, i, VL));
-        else
-          tmp = max(sqrt(pow(NL[i].first-NL[dst].first, 2.0) + pow(NL[i].second-NL[dst].second, 2.0)), DP[i][dst]);
-        if(tmp < min) min = tmp;
-      }
+  double min = 1500.0;
+	char MARK[100];
+	memset(MARK, '0', 100);
+  for(vector<int>::iterator itr = VL.begin(); itr != VL.end(); itr++){
+		MARK[*itr] = '1';
+  }
+	
+  for(int i = 0; i < N; i++){
+    if(i != dst && MARK[i] != '1'){
+    	double tmp = sqrt(pow(NL[dst].first-NL[i].first, 2.0) + pow(NL[dst].second-NL[i].second, 2.0));
+			if(tmp < min){
+				VL.push_back(i); 
+				tmp = max(tmp, path(0, i, VL));
+				if(tmp < min) min = tmp;
+				VL.pop_back(); 
+			}
     }
   }
-  DP[src][dst] = min;
+	DP[src][dst] = min;
   return min;
 }
 int main()
@@ -45,26 +53,25 @@ int main()
   int C = 0;
   cin >> C;
   for(int t = 0; t < C; t++){
-    int N = 0;
+    N = 0;
     cin >> N;
 
-    for(int i = 0; i < 100; i++){
-      for(int j = 0; j < 100; j++){
-        DP[i][j] = -1.0;
-      }
-    }
     for(int i = 0; i < N; i++){
-      int x = 0, y = 0;
+      double x = 0, y = 0;
       cin >> x >> y;
       NL[i] = MP(x,y);
     }
+		NL[N] = MP(1001.0, 1001.0);
+    vector<int> VL; VL.push_back(N);
+		double tmp = path(0, N, VL);
 
     double max = 0;
     for(int i = 1; i < N; i++){
-      vector<int> VL; VL.push_back(i);
-      double tmp = path(0, i, VL);
+      //vector<int> VL; VL.push_back(i);
+			tmp = DP[0][i];
+      //double tmp = path(0, i, VL);
       if(max < tmp) max = tmp; 
     }
-    printf("%f\n", max); 
+    printf("%0.2f\n", (round(max*100.0)/100.0)); 
   }
 }

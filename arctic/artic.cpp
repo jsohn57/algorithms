8a175fe2aee7a36626f7d5e9a740870af1376695
junghoon sohn <junghoon.sohn@samsung.com>
2015-05-27 12:01:00 +0900

    Arctic with better performance solved  On branch master  Changes to be committed:    (use "git reset HEAD <file>..." to unstage)
    	modified:   arctic/artic.cpp

--- arctic/artic.cpp
+++ arctic/artic.cpp
@@ -4,6 +4,7 @@
 #include <utility>
 #include <algorithm>
 #include <cmath>
+#include <cstdio>
 
 using namespace std;
 
@@ -14,25 +15,27 @@ int C;
 int N;
 vector<PAIR> L;
 
-bool is_reachable(int D)
+bool is_reachable(double D)
 {
 	set<PAIR> visited;
 	vector<PAIR> dest;
 	dest.push_back(L[0]);
 	while(!dest.empty()){
+    //printf("dest size = %lu\n", dest.size());
 		PAIR node = dest.back();	
 		dest.pop_back();
-		visited.insert(node);
-		for(vector<PAIR>::iterator it = L.begin(); it != L.end(); it++){
-			printf("sqrt = %f\n", sqrt(pow(node.first-it->first, 2.0)+pow(node.second-it->second, 2.0)));
-			if(visited.find(*it) == visited.end() && 
-				sqrt(pow(node.first-it->first, 2.0)+pow(node.second-it->second, 2.0)) < D){
-					printf("push_back!\n");
-					dest.push_back(node);
-			}
-		}
+    if(visited.find(node) == visited.end()){
+		  visited.insert(node);
+		  for(vector<PAIR>::iterator it = L.begin(); it != L.end(); it++){
+		    //printf("sqrt = %f\n", sqrt(pow(node.first-it->first, 2.0)+pow(node.second-it->second, 2.0)));
+        if(sqrt(pow(node.first-it->first, 2.0)+pow(node.second-it->second, 2.0)) < D){
+            //printf("push_back!\n");
+            dest.push_back(*it);
+        }
+		  }
+    }
 	}
-	printf("L size = %lu, visited list size = %lu\n", L.size(), visited.size());
+	//printf("L size = %lu, visited list size = %lu\n", L.size(), visited.size());
 	if(L.size() == visited.size()) return true;
 	else return false;
 }
@@ -40,17 +43,19 @@ int main()
 {
 	cin >> C;
 	for(int i = 0; i < C; i++){
+    L.clear();
 		cin >> N;
-		int x = 0, y = 0;
+    //printf("N = %d\n", N);
+		double x = 0.0, y = 0.0;
 		for(int j = 0; j < N; j++){
 			cin >> x >> y;
 			L.push_back(MP(x,y));
 		}
 
 		double D = 0.0, min = 0.0, max = 1500;
-		for(int i = 0; i < 1; i++){
+		for(int j = 0; j < 100; j++){
 			D = (min + max)/2;
-			printf("D = %f\n", D);
+		  //printf("min = %f, max = %f, D = %f\n", min, max, D);
 			if(is_reachable(D)){
 				//find better answer
 				max = D;

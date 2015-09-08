575cf4767e5048b99f998c2ea7ea25ee5d74e12d
Jung Hoon Sohn <jsohn57@gmail.com>
2015-09-08 10:45:30 +0900

    On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	modified:   poly.cpp

--- poly/poly.cpp
+++ poly/poly.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 
+int poly(int f, int r) //f = number of squares in the first row, r = remaining squares that can be used
+{
+	if(r == 1)
+}
 int main()
 {
-
+	int C = 0, n = 0;
+	cin >> C;
+	for(int i = 0; i < C; i++){
+		cin >> n;
+	}
 }

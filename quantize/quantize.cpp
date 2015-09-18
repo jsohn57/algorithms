5f37e7f7738e521ad111dcf65ded4691520b9072
Jung Hoon Sohn <junghoon.sohn@samsung.com>
2015-09-18 15:30:33 +0900

    On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	modified:   quantize.cpp

--- quantize/quantize.cpp
+++ quantize/quantize.cpp
@@ -1,26 +1,51 @@
 #include <iostream>
 #include <algorithm>
+#include <cmath>
 
 using namespace std;
 
-double avg[101][101];
+int avg[101][101];
 int A[101];
-int DP[101][101];
+long long DP[101][101];
+int C, N, S;
 
-double quantize(int i, int s) // i = array index, s = number of usable numbers remaining
+long long quantize(int i, int s) // i = starting array index, s = number of usable numbers remaining
 {
-
+  if(N == s)
+    return 0;
+  if(s == 1){
+    long long part_sum = 0;
+    for(int j = i; j < N; j++)
+      part_sum += (avg[i][N-1]-A[j])*(avg[i][N-1]-A[j]);
+    return part_sum;
+  }
+ 
+  long long &ret = DP[i][s];
+  if(ret == -1){
+    long long min_val = 1000000;
+    for(int j = i; j <= N-s; j++){
+      long long part_sum = 0;
+      for(int k = i; k <= j; k++){
+        part_sum += (avg[i][j]-A[k])*(avg[i][j]-A[k]);
+      }
+      min_val = min(min_val, part_sum + quantize(j+1, s-1));
+    }
+    ret = min_val;
+  }
+  return ret;
 }
 
 int main()
 {
-  int C = 0, N = 0, S = 0;
+  C = 0;
   cin >> C;
   for(int c = 0; c < C; c++){
-    memset(A, 0, 101);
+    N = 0; S = 0;
     for(int i = 0; i < 101; i++){
+      A[i] = 0;
       for(int j = 0; j < 101; j++){
         avg[i][j] = 0;
+        DP[i][j] = -1;
       }
     }
     cin >> N >> S;
@@ -39,14 +64,15 @@ int main()
     // pre-calculate the averages
     for(int i = 0; i < N; i++){
       for(int j = i+1; j < N; j++){
-        avg[i][j] = (avg[i][j-1]*double(j-i)+A[j])/double(j-i+1);
+        avg[i][j] = (int)round((double)(avg[i][j-1]*(j-i)+A[j])/(double)(j-i+1));
       }
     }
 
     for(int i = 0; i < N; i++){
       for(int j = 0; j < N; j++){
-        printf("avg[%d][%d] = %.3f\n", i, j, avg[i][j]);
+        printf("avg[%d][%d] = %d\n", i, j, avg[i][j]);
       }
     }
+    printf("ans = %lld\n", quantize(0, (S>N)?N:S));
   }
 }

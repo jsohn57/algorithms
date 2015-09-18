c2d3f131fe6a43b77f582e70baa71b3efff7c9e3
Jung Hoon Sohn <junghoon.sohn@samsung.com>
2015-09-18 17:33:27 +0900

    On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	modified:   quantize.cpp

--- quantize/quantize.cpp
+++ quantize/quantize.cpp
@@ -11,23 +11,25 @@ int C, N, S;
 
 long long quantize(int i, int s) // i = starting array index, s = number of usable numbers remaining
 {
-  if(N == s)
-    return 0;
   if(s == 1){
     long long part_sum = 0;
-    for(int j = i; j < N; j++)
+    for(int j = i; j < N; j++){
       part_sum += (avg[i][N-1]-A[j])*(avg[i][N-1]-A[j]);
+      printf("i = %d, j = %d, part_sum = %lld\n", i, j, part_sum);
+    }
     return part_sum;
   }
  
   long long &ret = DP[i][s];
   if(ret == -1){
     long long min_val = 1000000;
-    for(int j = i; j <= N-s; j++){
+    for(int j = i; j <= (N-s); j++){
       long long part_sum = 0;
       for(int k = i; k <= j; k++){
         part_sum += (avg[i][j]-A[k])*(avg[i][j]-A[k]);
       }
+      if(i == 3)
+        printf("part_sum = %lld, quantize_sum = %lld, j+1 = %d, s-1 = %d\n", part_sum, quantize(j+1, s-1), j+1, s-1);
       min_val = min(min_val, part_sum + quantize(j+1, s-1));
     }
     ret = min_val;
@@ -65,14 +67,15 @@ int main()
     for(int i = 0; i < N; i++){
       for(int j = i+1; j < N; j++){
         avg[i][j] = (int)round((double)(avg[i][j-1]*(j-i)+A[j])/(double)(j-i+1));
+        //avg[i][j] = (avg[i][j-1]*(j-i)+A[j])/(j-i+1);
       }
     }
 
+    printf("%lld\n", quantize(0, (S>N)?N:S));
     for(int i = 0; i < N; i++){
       for(int j = 0; j < N; j++){
-        printf("avg[%d][%d] = %d\n", i, j, avg[i][j]);
+        printf("Avg[%d][%d] = %d\n", i, j, avg[i][j]);
       }
     }
-    printf("ans = %lld\n", quantize(0, (S>N)?N:S));
   }
 }

6b5d4d8448470a5a7d722448b8a976386e9c598c
Jung Hoon Sohn <junghoon.sohn@samsung.com>
2015-10-05 17:50:58 +0900

    On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	modified:   klis.cpp

--- klis/klis.cpp
+++ klis/klis.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
+#include <cstring>
+#include <pair>
 
 using namespace std;
 
 unsigned int A[501];
+unsigned int IDX[501];
 int DP[501];
 int C_DP[501][501];
 
@@ -23,6 +27,25 @@ int lis(int idx)
   return ret;
 }
 
+void reconstruct(int idx, int length, int order, vector<int> v)
+{
+  vector<int, int> pv;
+  for(int i = idx+1; i < N; i++){
+    pv.push_back(make_pair(A[idx], idx)); 
+  }
+  sort(pv.begin(), pv.end());
+  int k = 0;
+  for(Iterator it = pv.begin(); it != pv.end(); it++){
+    k = order;
+    order -= count(length-1, it->second);
+    if(order <= 0){
+      v.push_back(it->first);
+      reconstruct(i, length-1, k, v);
+      v.pop_back();
+    }
+  }
+}
+
 int count(int l, int idx)
 {
   if(l == 1)
@@ -40,12 +63,13 @@ int count(int l, int idx)
 
 int main()
 {
-  C = 0; N = 0;
+  C = 0; N = 0; K = 0;
   cin >> C;
   for(int c = 0; c < C; c++){
     for(int i = 0; i < 501; i++){
       A[i] = -1;
       DP[i] = -1;
+      IDX[i] = -1;
       for(int j = 0; j < 501; j++){
         C_DP[i][j] = -1;
       }
@@ -53,18 +77,39 @@ int main()
     cin >> N >> K;
     for(int i = 0; i < N; i++){
       cin >> A[i];
+      IDX[A[i]] = i;
     }
     
     int length = lis(-1)-1;
     printf("%d\n", length);
-		int ctr = 0;
-		int 
+		int ctr = -1;
+		int order = 0;
+    unsigned int sorted_A[501];
+    memcpy(sorted_A, A, sizeof(int)*N);
+    sort(sorted_A, sorted_A+N);
+
 		while(K > 0){
-			K = K-count(length, ctr++);	
+      order = K;
+			K = K-count(length, IDX[sorted_A[++ctr]]);	
 		}
 		
-    for(int i = 0; i < N; i++){
-      printf("idx = %d, count = %d\n", i, count(length, i));
+    //printf("order = %d, val = %d, idx = %d\n", order, sorted_A[ctr], IDX[sorted_A[ctr]]);
+    vector<int> v;
+    vector< vector<int> > vv;
+    reconstruct(IDX[sorted_A[ctr]], length, v, vv);
+    sort(vv.begin(), vv.end());
+    v = vv[order-1];
+    for(int i = 0; i < v.size(); i++){
+      printf("%d ", v[i]);
     }
+    printf("\n");
+    /*
+    for(int i = 0; i < vv.size(); i++){
+      for(int j = 0; j < vv[i].size(); j++){
+        printf("%d ", vv[i][j]);
+      }
+      printf("\n\n");
+    }
+    */
   }
 }

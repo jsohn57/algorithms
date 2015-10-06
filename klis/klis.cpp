cb6a5b3684679b1eabe2229a3d730a143a047828
Jung Hoon Sohn <junghoon.sohn@samsung.com>
2015-10-06 19:05:28 +0900

    On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	modified:   klis.cpp

--- klis/klis.cpp
+++ klis/klis.cpp
@@ -2,16 +2,16 @@
 #include <algorithm>
 #include <vector>
 #include <cstring>
-#include <pair>
+#include <utility>
 
 using namespace std;
 
 unsigned int A[501];
-unsigned int IDX[501];
 int DP[501];
-int C_DP[501][501];
+long long C_DP[501][501];
 
-int C, N, K;
+int C, N;
+long long K;
 
 int lis(int idx)
 {
@@ -27,30 +27,11 @@ int lis(int idx)
   return ret;
 }
 
-void reconstruct(int idx, int length, int order, vector<int> v)
-{
-  vector<int, int> pv;
-  for(int i = idx+1; i < N; i++){
-    pv.push_back(make_pair(A[idx], idx)); 
-  }
-  sort(pv.begin(), pv.end());
-  int k = 0;
-  for(Iterator it = pv.begin(); it != pv.end(); it++){
-    k = order;
-    order -= count(length-1, it->second);
-    if(order <= 0){
-      v.push_back(it->first);
-      reconstruct(i, length-1, k, v);
-      v.pop_back();
-    }
-  }
-}
-
-int count(int l, int idx)
+long long count(int l, int idx)
 {
   if(l == 1)
     return 1;
-  int &ret = C_DP[l][idx];
+  long long &ret = C_DP[l][idx];
   if(ret == -1){
     ret = 0;
     for(int i = idx+1; i < N; i++){
@@ -61,6 +42,39 @@ int count(int l, int idx)
   return ret;
 }
 
+
+void reconstruct(int idx, int length, long long order, vector<int> &v)
+{
+  //printf("reconstruct: idx = %d, length = %d, order = %d\n", idx, length, order);
+  vector< pair<int, int> > pv;
+  int tmp;
+  (idx < 0)? tmp = 0 : tmp = A[idx];
+  for(int i = idx+1; i < N; i++){
+    if(tmp < A[i])
+      pv.push_back(make_pair(A[i], i)); 
+  }
+  sort(pv.begin(), pv.end());
+
+  if(length == 1 && order > 1){
+    //printf("order when length = 1 : %d\n", order);
+    vector< pair<int, int> >::iterator it = pv.begin();
+    while(order-- > 1) it++;
+    v.push_back(it->first);
+    return; 
+  }
+  long long k = 0;
+  for(vector< pair<int, int> >::iterator it = pv.begin(); it != pv.end(); it++){
+    k = order;
+    order -= count(length-1, it->second);
+    //printf("k = %d, order = %d, idx = %d, count = %d\n", k, order, it->second, count(length-1, it->second));
+    if(order <= 0){
+      v.push_back(it->first);
+      reconstruct(it->second, length-1, k, v);
+      break;
+    }
+  }
+}
+
 int main()
 {
   C = 0; N = 0; K = 0;
@@ -69,7 +83,6 @@ int main()
     for(int i = 0; i < 501; i++){
       A[i] = -1;
       DP[i] = -1;
-      IDX[i] = -1;
       for(int j = 0; j < 501; j++){
         C_DP[i][j] = -1;
       }
@@ -77,28 +90,13 @@ int main()
     cin >> N >> K;
     for(int i = 0; i < N; i++){
       cin >> A[i];
-      IDX[A[i]] = i;
     }
     
     int length = lis(-1)-1;
     printf("%d\n", length);
-		int ctr = -1;
-		int order = 0;
-    unsigned int sorted_A[501];
-    memcpy(sorted_A, A, sizeof(int)*N);
-    sort(sorted_A, sorted_A+N);
-
-		while(K > 0){
-      order = K;
-			K = K-count(length, IDX[sorted_A[++ctr]]);	
-		}
 		
-    //printf("order = %d, val = %d, idx = %d\n", order, sorted_A[ctr], IDX[sorted_A[ctr]]);
     vector<int> v;
-    vector< vector<int> > vv;
-    reconstruct(IDX[sorted_A[ctr]], length, v, vv);
-    sort(vv.begin(), vv.end());
-    v = vv[order-1];
+    reconstruct(-1, length+1, K, v);
     for(int i = 0; i < v.size(); i++){
       printf("%d ", v[i]);
     }
@@ -112,4 +110,5 @@ int main()
     }
     */
   }
+  return 0;
 }

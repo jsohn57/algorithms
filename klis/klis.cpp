c63308bee6c05e3f882d4c5adefe63900a456107
Jung Hoon Sohn <junghoon.sohn@samsung.com>
2015-10-08 17:47:17 +0900

    On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	modified:   klis.cpp

--- klis/klis.cpp
+++ klis/klis.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-unsigned int KMAX = 40000;
+unsigned int KMAX = 2147483647; //INT_MAX
 unsigned int A[501];
 int DP[501];
 int C_DP[501];
@@ -50,14 +50,14 @@ unsigned int count(int l, int idx)
 
 unsigned int count(int idx)
 {
-  if(l == 1)
+  if(lis(idx) == 1)
     return 1;
   int &ret = C_DP[idx];
   if(ret == -1){
     ret = 0;
     for(int i = idx+1; i < N; i++){
-      if(A[i] > A[idx] && (l-1) == lis(i)){
-        ret = min(KMAX, ret+count(l-1, i));
+      if(A[i] > A[idx] && lis(idx)-1 == lis(i)){
+        ret = min<long long>(KMAX, (long long)ret+count(i));
         //ret += count(l-1, i); 
       }
     }
@@ -68,29 +68,28 @@ unsigned int count(int idx)
 
 void reconstruct(int idx, int order, vector<int> &v)
 {
-  //printf("reconstruct: idx = %d, length = %d, order = %d\n", idx, length, order);
+  //printf("reconstruct: idx = %d, order = %d\n", idx, order);
 	
 	if(idx != -1)
 		v.push_back(A[idx]);
 
   vector< pair<int, int> > pv;
-  int tmp;
-  (idx < 0)? tmp = 0 : tmp = A[idx];
   for(int i = idx+1; i < N; i++){
-    if(tmp < A[i])
+    if((idx == -1 || A[idx] < A[i]) && (lis(idx) == lis(i)+1))
       pv.push_back(make_pair(A[i], i)); 
   }
   sort(pv.begin(), pv.end());
 
-  int k = 0;
+  //int k = 0;
   for(vector< pair<int, int> >::iterator it = pv.begin(); it != pv.end(); it++){
-    k = order;
-    order -= count(it->second);
-    //printf("k = %d, order = %d, idx = %d, count = %d\n", k, order, it->second, count(length-1, it->second));
-    if(order <= 0){
+    //printf("k = %d, order = %d, idx = %d, count = %d\n", k, order, it->second, count(it->second));
+    if(order < count(it->second)){
       //v.push_back(it->first);
-      reconstruct(it->second, k, v);
+      reconstruct(it->second, order, v);
       break;
+    }
+    else{
+      order -= count(it->second);
     }
   }
 }
@@ -103,9 +102,12 @@ int main()
     for(int i = 0; i < 501; i++){
       A[i] = -1;
       DP[i] = -1;
+      C_DP[i] = -1;
+      /*
       for(int j = 0; j < 501; j++){
         C_DP[i][j] = -1;
       }
+      */
     }
     cin >> N >> K;
     for(int i = 0; i < N; i++){
@@ -116,7 +118,7 @@ int main()
     printf("%d\n", length);
 		
     vector<int> v;
-    reconstruct(-1, length+1, K, v);
+    reconstruct(-1, K-1, v);
     for(int i = 0; i < v.size(); i++){
       printf("%d ", v[i]);
     }

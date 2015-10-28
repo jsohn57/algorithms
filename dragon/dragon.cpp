aac2a60827d9dc392d6bbc58599baed0ca65b854
Jung Hoon Sohn <junghoon.sohn@samsung.com>
2015-10-28 21:10:43 +0900

    On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	modified:   dragon.cpp

--- dragon/dragon.cpp
+++ dragon/dragon.cpp
@@ -3,24 +3,43 @@
 
 using namespace std;
 
-int CL[51];
+int XL[51];
+int YL[51];
 
-void curve(const string in, int gen)
+void calc_length()
 {
-	if(gen == 0){
+  XL[0] = 1; XL[1] = 4; YL[0] = 0; YL[1] = 1;
+  for(int i = 2; i < 51; i++){
+    XL[i] = 2*XL[i-1]+2;
+    YL[i] = 2*YL[i-1]+2;
+  }
+}
+
+void curve(const string in, int gen, int count)
+{
+	if(count == 0 && gen == 0){
 		cout << in;	
 		return;
 	}
 	int len = in.length();
 	for(int i = 0; i < len; i++){
 		if(in[i] == 'X'){
-			curve("X+YF", gen-1);
+      if(XL[gen] >= count)
+			  curve("X+YF", gen-1, count);
+      else
+        count -= XL[gen];
 		}
 		else if(in[i] == 'Y'){
-			curve("FX-Y", gen-1);	
+      if(YL[gen] >= count)
+			  curve("FX-Y", gen-1, count);	
+      else
+        count -= YL[gen];
 		}
 		else{
-			cout << in[i];	
+      if(count > 0)
+        count--;
+      else
+			  cout << in[i];	
 		}
 	}
 }
@@ -28,10 +47,13 @@ void curve(const string in, int gen)
 int main()
 {
 	int C, N, P, L;
-	C << cin;
-	N << P << L << cin;
+  cin >> C;
+  cin >> N >> P >> L;
 	for(int c = 0; c < C; c++){
-		curve("FX", N);
+    memset(XL, 0, sizeof(int)*51);
+    memset(YL, 0, sizeof(int)*51);
+    calc_length();
+		curve("FX", N, P);
 	}
 	cout << endl;
 	return 0;

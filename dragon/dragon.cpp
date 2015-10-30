f800f08bd43613586da3256462b7145894eeb1ee
Jung Hoon Sohn <jsohn57@gmail.com>
2015-10-30 10:02:48 +0900

    On branch master  Your branch is behind 'origin/master' by 1 commit, and can be fast-forwarded.    (use "git pull" to update your local branch)
     Changes to be committed:
    	modified:   dragon.cpp

--- dragon/dragon.cpp
+++ dragon/dragon.cpp
@@ -3,7 +3,17 @@
 
 using namespace std;
 
-int CL[51];
+int XL[51];
+int YL[51];
+void calc_length(int gen)
+{
+	XL[0] = 1; XL[1] = 4;
+	YL[0] = 0; YL[1] = 1;  
+	for(int i = 2; i < gen; i++){
+		XL[i] = XL[i-1]*2+2
+		YL[i] = YL[i-1]*2+2
+	}
+}
 
 void curve(const string in, int gen)
 {
@@ -31,6 +41,8 @@ int main()
 	C << cin;
 	N << P << L << cin;
 	for(int c = 0; c < C; c++){
+		memset(CL, 0, sizeof(int)*51);
+		calc_length(N);
 		curve("FX", N);
 	}
 	cout << endl;

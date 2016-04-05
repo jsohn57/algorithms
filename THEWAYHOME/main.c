db708631e6da11f81b2d9a749c7032fbfcc4eb4a
Jung Hoon Sohn <jsohn57@gmail.com>
2016-04-06 08:50:22 +0900

    On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	modified:   THEWAYHOME/main.c

--- THEWAYHOME/main.c
+++ THEWAYHOME/main.c
@@ -12,33 +12,38 @@ int E[MAX_N][MAX_N];
 int D[MAX_N];
 int visited[MAX_N];
 
+int find_min()
+{
+    int next = 0;
+    int min_dist = MAX_N*MAX_D;
+    for(int i = 1; i <= N; i++){
+        if(!visited[i] && D[i] < min_dist){
+            min_dist = D[i];
+            next = i;
+        }
+    }
+    return next;
+}
+
 int Dijkstra()
 {
-    int cur = N;
     int next = 0;
     int cur_dist = 0;
-    int min_dist = MAX_D;
     int v_ctr = 1;
 
     while(v_ctr <= N){
-        visited[cur] = 1;
-        min_dist = MAX_D;
+        next = find_min();
+        visited[next] = 1;
+        v_ctr++;
+        
+        if(next == 1) break;
         for(int i = 1; i <= N; i++){
-            if(i != cur){
-                if(E[cur][i] < min_dist && !visited[i]){
-                    min_dist = E[cur][i];
-                    next = i;
-                }
-                if(D[i] > cur_dist + E[cur][i]){
-                    D[i] = cur_dist + E[cur][i];
-                }
+            if(!visited[i]){
+                cur_dist = D[next] + E[next][i];
+                if(cur_dist < D[i]) D[i] = cur_dist;
             }
         }
-        cur_dist += min_dist;
-        cur = next;
-        v_ctr++;
     }
-    
     return D[1];
 }
 
@@ -46,17 +51,13 @@ int main()
 {
     memset(visited, 0, sizeof(int)*MAX_N);
     memset(E, MAX_D, sizeof(int)*MAX_N*MAX_N);
-    //memset(D, MAX_D, sizeof(int)*MAX_N);
-    
+    //memset(D, MAX_N*MAX_D, sizeof(int)*MAX_N);
+    for(int i = 0; i < MAX_N; i++){
+        D[i] = MAX_N*MAX_D;
+    }
     scanf("%d %d", &T, &N);
     int s, e, d;
     
-    
-    for(int i = 1; i < MAX_N; i++){
-        D[i] = MAX_N*MAX_D;
-    }
- 
-    D[N] = 0;
     for(int t = 0; t < T; t++){
         scanf("%d %d %d", &s, &e, &d);
         if(E[s][e] > d){
@@ -65,6 +66,7 @@ int main()
         }
     }
     
+    D[N] = 0;
     int ans = Dijkstra();
     printf("%d\n", ans);
     

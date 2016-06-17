3ec97a7e1a26f76fc43f93b5efc9ac9f851c8ba3
Jung Hoon Sohn <junghoon.sohn@samsung.com>
2016-06-17 19:25:54 +0900

    On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	modified:   blockgame/blockgame/main.c

--- blockgame/blockgame/main.c
+++ blockgame/blockgame/main.c
@@ -3,6 +3,7 @@
 
 char c_board[201][201];
 int board[201][201];
+int f_board[100][100];
 int visited[201 * 201];
 int end_blocks[100 * 100];
 int N, M;
@@ -28,17 +29,20 @@ int main()
 
 	scanf("%d", &N);
 	scanf("%d", &M);
-	scanf("%lf", &K);
+	scanf("%lf\n", &K);
+    //scanf("%c", &newline);  // scanf("%c") does not remove the leading whitespace or new line
 
 	memset(c_board, 0, sizeof(char) * 201 * 201);
     memset(visited, 0, sizeof(int) * 201 * 201);
 
 	// input
 	for (int i = 0; i <= 2 * M; i++){
+        char tmp;
 		for (int j = 0; j <= 2 * N; j++){
-            
-			scanf("%c", &c_board[i][j]);
+            scanf("%c", &tmp);
+            c_board[i][j] = tmp;
 		}
+        scanf("%c", &tmp); // for new line
 	}
 
     for(int i = 0; i <= 2*M; i++){
@@ -54,6 +58,53 @@ int main()
         }
         printf("\n");
     }
+    
+    int i_ctr = M-1, j_ctr = 0;
+    for(int i = 0; i <= 2*M; i++){
+        if(i%2 == 0) continue;
+        for(int j = 0; j <= 2*N; j++){
+            if(j%2 == 1) f_board[i_ctr][j_ctr++] = board[i][j];
+        }
+        if(i%2 == 1){
+            i_ctr--;
+            j_ctr = 0;
+        }
+    }
+    
+    printf("\n");
+    for(int i = 0; i < M; i++){
+        for(int j = 0; j < N; j++){
+            printf("%d ", f_board[i][j]);
+        }
+        printf("\n");
+    }
+    
+    double dst_x = 0, dst_y = 0;
+    double src_x = K, src_y = 0;
+    double b;
+    
+    // first arrow
+    b = src_x/2.0;
+    dst_x = 0; dst_y = K/2.0;
+    for(int i = src_y; i < dst_y; i++){
+        printf("x = %lf, y = %d\n", 2*i+K, i);
+    }
+    src_x = dst_x; src_y = dst_y;
+    
+    // second arrow
+    dst_x = 2*dst_y-K; dst_y = M-1;
+    for(int i = src_y; i < dst_y; i++){
+        printf("x = %lf, y = %d\n", 2*i-K, i);
+    }
+    src_x = dst_x; src_y = dst_y;
+    
+    // third arrow
+    dst_x = N-1; dst_y =
+    for(int i = src_y; i < dst_y; i++){
+        printf("x = %lf, y = %d\n", 2*i-K, i);
+    }
+    src_x = dst_x; src_y = dst_y;
+    
     
     return 0;
 }
